2da7b3e30d458599105ba551d0e7db40de9a0806
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2025-04-15 22:29:04 +0700

    master

--- components/sidebar.php
+++ components/sidebar.php
@@ -4,13 +4,7 @@
     <div class="sidebar-action">
       <h3>Kursus Mobil</h3>
       <button class="toggle-btn" onclick="toggleSidebar()">
-        <span class="icon">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 512 512">
-            <line x1="88" y1="152" x2="424" y2="152" style="fill:none;stroke:#000;stroke-linecap:round;stroke-miterlimit:10;stroke-width:48px" />
-            <line x1="88" y1="256" x2="424" y2="256" style="fill:none;stroke:#000;stroke-linecap:round;stroke-miterlimit:10;stroke-width:48px" />
-            <line x1="88" y1="360" x2="424" y2="360" style="fill:none;stroke:#000;stroke-linecap:round;stroke-miterlimit:10;stroke-width:48px" />
-          </svg>
-        </span>
+        <ion-icon name="menu"></ion-icon>
       </button>
     </div>
     <a href="../dashboard.php">Dashboard</a>
