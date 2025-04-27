a9aff23723b51340d58ee2f4deb1ab861cd410bc
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2025-04-27 22:27:36 +0700

    master

--- components/landingPage/header.php
+++ components/landingPage/header.php
@@ -7,26 +7,28 @@
     </a>
     <ul class="nav">
       <li class="list active"><a href="" class="link">Berada</a></li>
-      <li class="list"><a href="" class="link">Materi</a></li>
+      <li class="list"><a href="../../courses/all.php" class="link">Kursus</a></li>
       <li class="list"><a href="" class="link">Tentang</a></li>
     </ul>
     <ul class="action">
       <li class="list">
-        <?php
-
-        $action = isset($_SESSION['auth']) ? "../../auth/logout.php" : "../../auth/login.php";
-
-        ?>
-        <a href="<?= $action ?>" class="btn">
+        <a href="<?= isset($_SESSION['auth']) ? '../../auth/logout.php' : '../../auth/login.php' ?>" class="btn">
           <?php if (isset($_SESSION['auth'])): ?>
             <?= $_SESSION['name'] ?>
           <?php else: ?>
             Masuk
           <?php endif ?>
-
         </a>
       </li>
-      <li class="list"><a href="#" class="btn primary">Ambil Lisensi</a></li>
+      <li class="list">
+        <a href="<?= isset($_SESSION['auth']) ? '../../dashboard.php' : '../../auth/register.php' ?>" class="btn primary">
+          <?php if (isset($_SESSION['auth'])): ?>
+            Dashboard
+          <?php else: ?>
+            Registrasi
+          <?php endif ?>
+        </a>
+      </li>
     </ul>
   </header>
 <?php } ?>
