498d625394e27b6ab4a643af1399c4067ec2bbb7
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2025-04-30 20:28:34 +0700

    master

--- auth/login.php
+++ auth/login.php
@@ -26,8 +26,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
           break;
       }
     } else {
-      header("Location: login.php");
       $error = "Email dan password tidak sesuai.";
+      header("Location: login.php");
     }
   }
 }
