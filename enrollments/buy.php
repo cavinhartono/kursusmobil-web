a5568ce74a0ad28eff5f3476ac9b245ff3981a4b
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2025-05-04 19:42:24 +0700

    master

--- enrollments/buy.php
+++ enrollments/buy.php
@@ -19,9 +19,9 @@ $Course = mysqli_query(
     WHERE id = $course_id"
 )->fetch_object();
 
-$connect->query(
-  "INSERT INTO Orders(user_id, course_id) VALUES ($user_id, $course_id)"
-);
+// $connect->query(
+//   "INSERT INTO Orders(user_id, course_id) VALUES ($user_id, $course_id)"
+// );
 
 ?>
 
@@ -31,39 +31,44 @@ $connect->query(
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../assets/css/landing_page/enrollment/style.css">
   <title>Metode Pembelian</title>
 </head>
 
-<body>
-  <h1>Metode Pembayaran</h1>
-  <p>Nama: <?= $Course->name ?></p>
-  <p>Harga: <?= number_format($Course->price, 0, "", ".") ?></p>
-  <h1>
-    Pengemudi
-  </h1>
-  <p>Nama: <?= $User->name ?></p>
-  <p>Email: <?= $User->email ?></p>
-  <p>Nomor Telepon: <?= $User->phone ?></p>
-  <div id="display"></div>
-  <form method="post">
-    <select name="metode" id="metode" onchange="toggleMetode()" required>
-      <option value="">-- Pilih --</option>
-      <option value="debit">Kartu Debit</option>
-      <option value="qris">QRIS</option>
-    </select>
+<body style="display: grid; place-content: center; min-height: 100vh">
+  <div class="container pay" style="box-shadow: 0px 8px 45px rgba(34, 34, 34, 0.1);">
+    <h1>Metode Pembayaran</h1>
+    <p>Nama: <?= $Course->name ?></p>
+    <p>Harga: <?= number_format($Course->price, 0, "", ".") ?></p>
+    <h1>
+      Pengemudi
+    </h1>
+    <p>Nama: <?= $User->name ?></p>
+    <p>Email: <?= $User->email ?></p>
+    <p>Nomor Telepon: <?= $User->phone ?></p>
+    <div id="display" style="display: none;"></div>
+    <form method="post">
+      <select name="metode" id="metode" onchange="toggleMetode()" required>
+        <option value="">-- Pilih --</option>
+        <option value="debit">Kartu Debit</option>
+        <option value="qris">QRIS</option>
+      </select>
 
-    <div id="debit" style="display:none;">
-      <label>Masukan Kartu ID<sup>*</sup>:</label>
-      <input type="text" id="code" name="code" required>
-      <button type="button" onclick="getPayment()">Cari</button>
-      <button type="submit" name="create">Beli</button>
-    </div>
+      <div id="debit" style="display:none;">
+        <label>Masukan Kartu ID<sup>*</sup>:</label>
+        <div class="action">
+          <input type="text" id="code" name="code" required>
+          <button type="button" onclick="getPayment()">Cari</button>
+        </div>
+        <button type="submit" name="create">Beli</button>
+      </div>
 
-    <div id="qris" style="display: none;">
-      <?php $url = "192.168.43.45:8001/enrollments/pay.php?user_id=$_SESSION[auth]&price=$Course->price&course_id=$course_id&method=qris" ?>
-      <img src="https://chart.googleapis.com/chart?cht=qr&chs=250x250&chl=<?= urlencode($url) ?>" alt="QR Code">
-    </div>
-  </form>
+      <div id="qris" style="display: none;">
+        <?php $url = "192.168.43.45:8001/enrollments/pay.php?user_id=$_SESSION[auth]&price=$Course->price&course_id=$course_id&method=qris" ?>
+        <img src="https://api.qrserver.com/v1/create-qr-code/?data=<?= urlencode($url) ?>&size=250x250">
+      </div>
+    </form>
+  </div>
 
   <script>
     function getPayment() {
@@ -76,6 +81,7 @@ $connect->query(
         })
         .then(response => response.text())
         .then(result => {
+          document.querySelector("#display").style.display = "block";
           document.querySelector("#display").innerHTML = result;
         })
         .catch(error => console.error(error));
@@ -83,7 +89,7 @@ $connect->query(
 
     function toggleMetode() {
       var metode = document.getElementById('metode').value;
-      document.getElementById('qris').style.display = metode === 'qris' ? 'block' : 'none';
+      document.getElementById('qris').style.display = metode === 'qris' ? 'flex' : 'none';
       document.getElementById('debit').style.display = metode === 'debit' ? 'block' : 'none';
     }
   </script>
