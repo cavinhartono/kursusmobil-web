83829a7e668fd8aa9d82f0989176d106f6644293
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2025-05-05 23:35:27 +0700

    master

--- components/sidebar.php
+++ components/sidebar.php
@@ -8,16 +8,22 @@
       </button>
     </div>
     <a href="../dashboard.php">Dashboard</a>
-    <button class="dropdown" onclick="toggleDropdown()">Kelola <span>▼</span></button>
-    <div class="dropdown-content" id="dropdown-content">
-      <a href="../students/index.php">Pengemudi</a>
-      <a href="../instructors/index.php">Instruktur</a>
-      <a href="../courses/index.php">Kursus</a>
-      <a href="../cars/index.php">Mobil</a>
-    </div>
-    <a href="#">Jadwal (User)</a>
-    <a href="#">Penilaian (User)</a>
-    <a href="#">Sertifikat (User)</a>
-    <a href="#">Laporan</a>
+    <?php if ($_SESSION['roles'] == "admin"): ?>
+      <button class="dropdown" onclick="toggleDropdown()">Kelola <span>▼</span></button>
+      <div class="dropdown-content" id="dropdown-content">
+        <a href="../students/index.php">Pengemudi</a>
+        <a href="../instructors/index.php">Instruktur</a>
+        <a href="../courses/index.php">Kursus</a>
+        <a href="../cars/index.php">Mobil</a>
+      </div>
+      <a href="#">Laporan</a>
+    <?php elseif ($_SESSION['roles'] == "instructor"): ?>
+      <a href="#">Jadwal</a>
+      <a href="">Penilaian</a>
+    <?php else: ?>
+      <a href="#">Jadwal</a>
+      <a href="#">Penilaian</a>
+      <a href="#">Sertifikat</a>
+    <?php endif ?>
   </div>
 <?php } ?>
