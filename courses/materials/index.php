6bfa1d59bd0e5fa3eec5dfed3d66ff0ffb7f38f9
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2025-05-10 15:48:20 +0700

    master

--- courses/materials/index.php
+++ courses/materials/index.php
@@ -1,49 +1,3 @@
-<?php
-function modalForMaterial($type, $fields = [], $actionName = '', $id = '', $values = [])
-{
-  $modalId = $type . 'Modal' . ($id ? $id : '');
-  $submitName = $type === 'create' ? 'add' : 'update';
-  $action = getFormAction($type);
-?>
-  <div id="<?= $modalId ?>" class="modal">
-    <div class="modal-content">
-      <span class="modal-close" onclick="closeModal('<?= $modalId ?>')">&times;</span>
-      <h3>
-        <?php switch ($type):
-          case 'create': ?>
-            <?= "Tambah Data $actionName" ?>
-            <?php break; ?>
-          <?php
-          default: ?>
-            <?= "Edit Data $actionName" ?>
-            <?php break; ?>
-        <?php endswitch ?>
-      </h3>
-      <form action="<?= $action ?>" method="POST" onsubmit="return submitForm()">
-        <?php if ($type === 'update'): ?>
-          <input type="hidden" name="id" value="<?= $id ?>">
-        <?php endif; ?>
-        <?php foreach ($fields as $field): ?>
-          <?php if ($field['name'] === 'content'): ?>
-            <label><?= $field['label'] ?>:</label>
-            <div id="editor"></div>
-            <input type="hidden" name="content" id="content">
-          <?php elseif ($field['type'] === 'hidden'): ?>
-            <input type="hidden" name="<?= $field['name'] ?>" value="<?= $field['value'] ?>">
-          <?php else: ?>
-            <label><?= $field['label'] ?>:</label>
-            <input
-              type="<?= $field['type'] ?>"
-              name="<?= $field['name'] ?>"
-              required>
-          <?php endif; ?>
-        <?php endforeach; ?>
-        <button type="submit" name="<?= $submitName ?>" class="btn primary"><ion-icon name="checkmark"></ion-icon></button>
-      </form>
-    </div>
-  </div>
-<?php } ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -66,12 +20,6 @@ $course = $connect->query("SELECT name FROM Courses WHERE id = $course_id")->fet
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?= $course->name ?></title>
   <link rel="stylesheet" href="../../assets/css/style.css" />
-  <link rel="stylesheet" href="../../assets/js/quilljs/quill.snow.css">
-  <style>
-    .modal-content {
-      width: 900px !important;
-    }
-  </style>
 </head>
 
 <body>
@@ -82,7 +30,6 @@ $course = $connect->query("SELECT name FROM Courses WHERE id = $course_id")->fet
     ['name' => 'title', 'label' => 'Judul', 'type' => 'text'],
     ['name' => 'content', 'label' => 'Isi Materi'],
   ];
-  modalForMaterial('create', $fields, 'Materi');
   ?>
 
   <?php sidebar() ?>
@@ -107,17 +54,6 @@ $course = $connect->query("SELECT name FROM Courses WHERE id = $course_id")->fet
             <?php
             $Materials = $connect->query("SELECT * FROM Materials WHERE course_id = $course_id");
 
-            $editData = null;
-            if (isset($_GET['edit'])) {
-              $id = (int) $_GET['edit'];
-              $data = mysqli_query($connect, "SELECT * FROM Materials WHERE id = $id")->fetch_assoc();
-
-              if ($data) {
-                modalForMaterial("update", $fields, "Materi", $id, $data);
-                echo "<script>window.onload = () => openModal('updateModal$id');</script>";
-              }
-            }
-
             if (isset($_GET['delete'])) {
               $id = $_GET['delete'];
               $connect->query("DELETE FROM Materials WHERE id=$id");
@@ -132,8 +68,8 @@ $course = $connect->query("SELECT name FROM Courses WHERE id = $course_id")->fet
                   <td><?= $material->title ?></td>
                   <td style="text-align: center;"><?= timeAgo($material->uploaded_at) ?></td>
                   <td>
-                    <a href="?id=<?= $course_id ?>&edit=<?= $material->id ?>" class="btn warning"><ion-icon name="create-outline"></ion-icon></a>
-                    <a href="?id=<?= $course_id ?>&delete=<?= $material->id ?>" class="btn danger" onclick="return confirm('Yakin hapus?')"><ion-icon name="trash-bin-outline"></ion-icon></a>
+                    <a href="edit.php?id=<?= $material->id ?>" class="btn warning"><ion-icon name="create-outline"></ion-icon></a>
+                    <a href="?delete=<?= $material->id ?>" class="btn danger" onclick="return confirm('Yakin hapus?')"><ion-icon name="trash-bin-outline"></ion-icon></a>
                     <a href="view.php?id=<?= $course_id ?>&page=<?= $material->id ?>" class="btn danger">View</a>
                   </td>
                 </tr>
@@ -153,17 +89,6 @@ $course = $connect->query("SELECT name FROM Courses WHERE id = $course_id")->fet
   <?php endif ?>
   <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
   <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-  <script src="../../assets/js/quilljs/quill.min.js"></script>
-  <script>
-    var quill = new Quill('#editor', {
-      theme: 'snow'
-    });
-
-    function submitForm() {
-      document.getElementById('content').value = quill.root.innerHTML;
-      return true;
-    }
-  </script>
   <script src="../../assets/js/script.js"></script>
 </body>
 
