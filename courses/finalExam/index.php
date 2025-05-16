553993e9277689e422ab6691ce2d7d0356a84683
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2025-05-16 19:37:41 +0700

    master

--- courses/finalExam/index.php
+++ courses/finalExam/index.php
@@ -6,8 +6,10 @@
 include_once("../../database/connect.php");
 
 $course_id = $_GET['id'];
+$user_id = $_GET['user_id'] ?? null;
 
 $course = $connect->query("SELECT name, quiz_json FROM Courses WHERE id = $course_id")->fetch_object();
+$user = $connect->query("SELECT name FROM Users WHERE id = $user_id")->fetch_object();
 $quizzes = json_decode($course->quiz_json, true);
 
 ?>
@@ -19,13 +21,16 @@ $quizzes = json_decode($course->quiz_json, true);
 </head>
 
 <body>
-  <form action="">
+  <form action="store.php" method="POST">
+    <input type="hidden" name="course_id" value="<?= $course_id ?>">
+    <input type="hidden" name="user_id" value="<?= $_SESSION['auth'] ?>">
     <?php foreach ($quizzes as $quiz): ?>
       <h3><?= htmlspecialchars($quiz['category']) ?></h3>
       <?php foreach ($quiz['items'] as $item): ?>
-        <label><input type="checkbox" name="nilai[]" <?= $_SESSION['roles'] === 'admin' ? 'disabled' : "" ?>> <?= htmlspecialchars($item) ?></label><br>
+        <label><input type="checkbox" name="nilai[<?= htmlspecialchars($quiz['category']) ?>][<?= htmlspecialchars($item) ?>]" <?= $_SESSION['roles'] === 'admin' ? 'disabled' : "" ?> value="1"> <?= htmlspecialchars($item) ?></label><br>
       <?php endforeach; ?>
     <?php endforeach; ?>
+    <button type="submit" name="get_result">Lanjut</button>
   </form>
 </body>
 
