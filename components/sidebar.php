9a7743a56dc52965e19777645ed44b3c140dcd1d
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2025-05-18 22:34:17 +0700

    master

--- components/sidebar.php
+++ components/sidebar.php
@@ -8,23 +8,25 @@
         <ion-icon name="menu"></ion-icon>
       </button>
     </div>
-    <a href="/dashboard.php" class="<?= str_contains($current, '/dashboard.php') ? 'active' : '' ?>">Dashboard</a>
+    <a href="/dashboard.php" class="link <?= str_contains($current, '/dashboard.php') ? 'active' : '' ?>"><ion-icon name="albums-outline"></ion-icon> Dashboard</a>
     <?php if ($_SESSION['roles'] === "admin"): ?>
-      <a href="/list_transaction.php" class="<?= str_contains($current, '/list_transaction.php') ? 'active' : '' ?>">Transaksi</a>
-      <button class="dropdown" onclick="toggleDropdown()">Kelola <span>▼</span></button>
+      <a href="/list_transaction.php" class="link <?= str_contains($current, '/list_transaction.php') ? 'active' : '' ?>"><ion-icon name="swap-horizontal-outline"></ion-icon> Transaksi</a>
+      <button class="dropdown" onclick="toggleDropdown()">
+        <div class="link"><ion-icon name="server-outline"></ion-icon> Kelola</div>
+        <span>▼</span>
+      </button>
       <div class="dropdown-content" id="dropdown-content">
-        <a href="/students/index.php" class="<?= str_contains($current, '/students') ? 'active' : '' ?>">Pengemudi</a>
-        <a href="/instructors/index.php" class="<?= str_contains($current, '/instructors') ? 'active' : '' ?>">Instruktur</a>
-        <a href="/courses/index.php" class="<?= str_contains($current, '/courses') ? 'active' : '' ?>">Kursus</a>
-        <a href="/cars/index.php" class="<?= str_contains($current, '/cars') ? 'active' : '' ?>">Mobil</a>
+        <a href="/students/index.php" class="link <?= str_contains($current, '/students') ? 'active' : '' ?>"><ion-icon name="people-outline"></ion-icon> Pengemudi</a>
+        <a href="/instructors/index.php" class="link <?= str_contains($current, '/instructors') ? 'active' : '' ?>"><ion-icon name="person-outline"></ion-icon> Instruktur</a>
+        <a href="/courses/index.php" class="link <?= str_contains($current, '/courses') ? 'active' : '' ?>"><ion-icon name="book-outline"></ion-icon> Kursus</a>
+        <a href="/cars/index.php" class="link <?= str_contains($current, '/cars') ? 'active' : '' ?>"><ion-icon name="car-sport-outline"></ion-icon> Mobil</a>
       </div>
-      <a href="/reports.php" class="<?= str_contains($current, '/reports') ? 'active' : '' ?>">Laporan</a>
+      <a href="/reports.php" class="link <?= str_contains($current, '/reports') ? 'active' : '' ?>"><ion-icon name="copy-outline"></ion-icon> Laporan</a>
     <?php elseif ($_SESSION['roles'] === "instructor"): ?>
-      <a href="/schedules" class="<?= str_contains($current, '/schedules') ? 'active' : '' ?>">Jadwal Penilaian</a>
+      <a href="/schedules/index.php" class="link <?= str_contains($current, '/schedules') ? 'active' : '' ?>"><ion-icon name="time-outline"></ion-icon> Jadwal Penilaian</a>
     <?php else: ?>
-      <a href="/schedules" class="<?= str_contains($current, '/schedules') ? 'active' : '' ?>">Jadwal</a>
-      <a href="/grades.php" class="<?= str_contains($current, '/grades') ? 'active' : '' ?>">Penilaian</a>
-      <a href="/certificate.php" class="<?= str_contains($current, '/certificate') ? 'active' : '' ?>">Sertifikat</a>
+      <a href="/schedules" class="link <?= str_contains($current, '/schedules') ? 'active' : '' ?>"><ion-icon name="time-outline"></ion-icon> Jadwal</a>
+      <a href="/certification/my_list.php" class="link <?= str_contains($current, '/certification') ? 'active' : '' ?>"><ion-icon name="school-outline"></ion-icon> Penilaian</a>
     <?php endif ?>
   </div>
 <?php } ?>
