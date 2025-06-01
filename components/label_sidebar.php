14824d20d50be6ea748f78a2bb507434a3fb1e1b
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2025-06-01 21:22:46 +0700

    master

--- components/label_sidebar.php
+++ components/label_sidebar.php
@@ -2,7 +2,7 @@
 { ?>
   <div class='fixed-header'>
     <h2><?= $title ?></h2>
-    <a href="/auth/logout.php" class="link <?= str_contains($_SERVER['REQUEST_URI'], '/auth/logout.php') ? 'active' : '' ?>">
+    <a href="/auth/logout.php" class="link <?= str_contains($_SERVER['REQUEST_URI'], '/auth/logout.php') ? 'active' : '' ?>" style="color: rgba(34, 34, 34, 0.75)">
       <?php if (isset($_SESSION['auth'])): ?>
         <?= $_SESSION['name'] ?>
       <?php endif ?>
