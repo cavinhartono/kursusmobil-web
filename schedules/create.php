553bd0a495ccba3c6509ec779f54f86aecc3e418
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2025-06-01 21:25:40 +0700

    master

--- schedules/create.php
+++ schedules/create.php
@@ -20,7 +20,7 @@ $Cars = $connect->query("SELECT * FROM Cars WHERE `status` = 'active'");
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../assets/css/style.css">
+  <link rel="stylesheet" href="../assets/css/dashboard/style.css">
   <title>Membuat Jadwal Ujian Praktek</title>
   <style>
     .fields {
